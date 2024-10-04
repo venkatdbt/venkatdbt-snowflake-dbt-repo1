b059a3f258f1b918b41d194e68540bf650c876aa
venkatdbt <venkatab1122@gmail.com>
2024-10-04 08:33:04 +0530

    my fist comit

---
+++ models/sahil3.sql
@@ -0,0 +1,5 @@
+select * from db_ecomm.sc_ecom.users 
+union all
+select * from db_ecomm.sc_ecom.users
+union all
+select * from db_ecomm.sc_ecom.users
---
+++ models/users_dbt.sql
@@ -0,0 +1,1 @@
+select * from db_ecomm.sc_ecom.users
